8ae13129f8aa01a39e9185328b0c5e012cd7c997
unknown <curtisdslone@gmail.com>
2024-06-05 10:31:25 +0200

    iframe test

--- RecentProducts/src/webparts/recentProducts/components/RecentProducts.tsx
+++ RecentProducts/src/webparts/recentProducts/components/RecentProducts.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import styles from './RecentProducts.module.scss';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel'
 import { IRecentProductsProps } from './IRecentProductsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { IDocument } from '../IDocument';
 import DocumentComponent from './DocumentComponent';
 
-
-export default class RecentProducts extends React.Component<IRecentProductsProps, {}> {
+ // Temp pdf for iframe
+ // https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-207.pdf
+export default class RecentProducts extends React.Component<IRecentProductsProps, {
+  showPanel: boolean;
+}> {
   constructor(props: IRecentProductsProps){
     super(props);
+    this.state = { 
+      showPanel: false
+    };
   } 
   
   public render(): React.ReactElement<IRecentProductsProps> {
@@ -28,8 +36,29 @@ export default class RecentProducts extends React.Component<IRecentProductsProps
       <div className={ styles.recentProducts }>
         <div className={styles.row}>
           {docs}
+          <div>
+            <DefaultButton
+              secondaryText='Open Panel'
+              onClick={this._setShowPanel(true)}
+              text='Open Panel'
+            />
+            <Panel
+              isOpen={ this.state.showPanel }
+              onDismiss={ this._setShowPanel(false) }
+              type={ PanelType.medium }
+              headerText='Document'
+            >
+              <iframe height='500' src='https://google.com' > </iframe>
+            </Panel>
+          </div>
         </div>
       </div>
     );
+  }
+
+  private _setShowPanel = (showPanel: boolean): () => void => {
+    return (): void => {
+      this.setState({showPanel});
+    };
   }
 }
